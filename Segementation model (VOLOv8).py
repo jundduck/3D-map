861d51e97a89f02d28ff8d3d8c44f97219581ed3
이준석 <ryan0826@naver.com>
2024-08-28 17:05:38 +0900

    Update Segementation model (VOLOv8).py

--- Segementation model (VOLOv8).py
+++ Segementation model (VOLOv8).py
@@ -1,72 +1,73 @@
-# install the required version of Ultralytics
-!pip install ultralytics==8.0.28
+from ultralytics import YOLO
 
+# load a pre-trained model
+model = YOLO("yolov8n-seg.pt")
 
-# Use Roboflow for Custom Datasets
-# dataset이 있다고 가정 (roboflow)
+# train the model
+model.train(data="data-seg.yaml", epochs=100, imgsz=640)
 
-import os
-from IPython.display import Image
-
-# Home Directory
-%cd {os.path.expanduser("~")}
-
-# predictions with YOLOv8
-!yolo task=segment mode=predict model=yolov8s-seg.pt conf=0.25 source='https://media.roboflow.com/user path' save=true
-
-Image(filename='user path', height=600)
-
-
-!mkdir {HOME}/datasets
-%cd {HOME}/datasets
-
-!pip install roboflow --quiet
-
-# Import Roboflow classes from the Roboflow library
-from roboflow import Roboflow
-
-# Create a Roboflow object using the user's API key
-rf = Roboflow(api_key="YOUR_API_KEY")
-
-# Access the specified workspace and project
-project = rf.workspace("alpaco5-f3woi").project("part-autolabeld")
-
-# Download data corresponding to version 5 of the project in YOLOv8 format
-dataset = project.version(5).download("yolov8")
-
-
-# Training YOLOv8 Instance Segmentation Model
-%cd {HOME}
-!yolo task=segment mode=train model=yolov8s-seg.pt  data={dataset.location}/data.yaml epochs=10 imgsz=640!ls {HOME}/runs/segment/train/
+# predict with the model
+results = model.predict(source="test.jpg")
 
 
 
-# Visualization
-%cd {HOME}
-Image(filename=f'{HOME}/runs/segment/train/confusion_matrix.png', width=600)
+# install YOLOv8
+pip install ultralytics
 
-%cd {HOME}
-Image(filename=f'{HOME}/runs/segment/train/results.png', width=600)
+# train model
+from ultralytics import YOLO
 
-%cd {HOME}
-Image(filename=f'{HOME}/runs/segment/train/val_batch0_pred.jpg', width=600)
+model = YOLO("yolov8n-seg.pt")
+model.train('data=mydata.yaml', epochs=10)
 
-
-
-
-# Predict with Your Custom YOLOv8 Instance Segmentation Model
-!yolo task=segment mode=val model={HOME}/runs/segment/train/weights/best.pt data={dataset.location}/data.yaml
-
-!yolo task=segment mode=predict model={HOME}/runs/segment/train/weights/best.pt conf=0.25 source={dataset.location}/test/images save=true
+# prediction
+results = model.predict(source='/content/test/')
 
 
 
-# Deploy the Trained Model to Roboflow
-# retrieve your workspace and project names
-rf = Roboflow(api_key="API_KEY")
-print(rf.workspace())
+!weget -0 data.zip https://universe.roboflow.com/custom data path
+
+import zipfile
+with zipfile.zipfile('/content/custom data.zip') as target_file:
+  target_file.extractall('/content/custom data')
+
+!cat /content/custom data/data.yaml
 
 
-# upload the trained model back to your desired project in Roboflow
-project = rf.workspace("roboflow-ngkro").project("car-parts-ecsse")
-project.version(3).deploy("yolov8", "runs/segment/train/")
+!pip install PyYAML
+import yaml
+data = { 'train': '/content/custom data/train/images'
+         'val': '/content/custom data/vaild/images'
+         'test': '/content/custom data/test/images'
+         'names': ['trash']
+         'nc': 1 }
+
+with open('/content/custom data/custom data.yaml', 'w') as f:
+  yaml.dump(data, f)
+
+with open('/content/custom data/custom data.yaml', 'r') as f:
+  taco_yaml = yaml.safe_load(f)
+  display(custom data_yaml)
+
+
+!pip install ultralytics
+import ultralytics
+ultralytics.checks()
+
+
+# Load YOLOv8n-seg
+from ultralytics import YOLO
+
+model = YOLO('yolov8n-seg.pt')
+
+print(type(model.names), len(model.names))
+print(model.names)
+
+
+
+model.train*(data='/content/custom data/custom data.yaml', epochs=50, patience=10, batch=32, imgsz=416)
+print(type(model.names), len(model.names))
+print(model.names)
+
+
+results = model.predict(source='/content/custom data/test/images', save = True)
